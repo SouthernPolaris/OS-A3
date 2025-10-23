29f7777b6e4c55bbf85f09c734028192bd408084
Asrith <asrithatluri@gmail.com>
2025-10-23 15:06:50 +1030

    mergesort initial impl

--- mergesort.c
+++ mergesort.c
@@ -9,10 +9,45 @@
 
 /* this function will be called by mergesort() and also by parallel_mergesort(). */
 void merge(int leftstart, int leftend, int rightstart, int rightend){
+
+	int i, j, k;
+	i = leftstart;
+	j = rightstart;
+	k = 0;
+
+	while(i <= leftend && j <= rightend){
+		if(A[i] <= A[j]){
+			B[k++] = A[i++];
+		} else {
+			B[k++] = A[j++];
+		}
+	}
+
+	while(i <= leftend){
+		B[k++] = A[i++];
+	}
+
+	while(j <= rightend){
+		B[k++] = A[j++];
+	}
+
+	for(i = leftstart, j = 0; i <= rightend; i++, j++){
+		A[i] = B[j];
+	}
 }
 
 /* this function will be called by parallel_mergesort() as its base case. */
 void my_mergesort(int left, int right){
+	if (left > right) {
+		return;
+	}
+
+	int mid = (left + right) / 2;
+
+	my_mergesort(left, mid);
+	my_mergesort(mid + 1, right);
+
+	merge(left, mid, mid + 1, right);
 }
 
 /* this function will be called by the testing program. */
